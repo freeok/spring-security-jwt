643a3c1c487650d9faba4793a6efedb811ec5c8b
freeok <51998152+freeok@users.noreply.github.com>
2023-03-07 17:33:36 +0800

    :bug: fix(TestController.getBookcase): 完善@PreAuthorize的EL表达式

--- src/main/java/work/pcdd/securityjwt/controller/TestController.java
+++ src/main/java/work/pcdd/securityjwt/controller/TestController.java
@@ -53,7 +53,7 @@ public class TestController {
      * Spring EL表达式应用举例：获取用户书架，只能获取token持有者的
      * 此处principal.username为用户的id，见CustomUser的super构造参数
      */
-    @PreAuthorize("principal.username == #userId.toString()")
+    @PreAuthorize("hasRole('user') and principal.username == #userId.toString()")
     @GetMapping("/f7")
     public R getBookcase(@RequestParam Long userId, Authentication authentication) {
         return R.ok(authentication);
